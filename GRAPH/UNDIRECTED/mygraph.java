752e88c4202007fefa248d2a26e3dc5593dfed5b
kamal <kamal08111998@gmail.com>
2022-02-01 23:07:15 +0530

    Updated

--- GRAPH/UNDIRECTED/mygraph.java
+++ GRAPH/UNDIRECTED/mygraph.java
@@ -26,8 +26,11 @@ public class mygraph {
         boolean[] vis = new boolean[n + 1];
         // System.out.println(hasPath(1, 7, vis));
         // System.out.println(allPath(1, 7, "", 0, vis));
-        pair ans = heaviestPath(1, 7, vis);
-        System.out.println(ans.path + "->" + ans.cost);
+        // pair ans = heaviestPath(1, 7, vis);
+        // System.out.println(ans.path + "->" + ans.cost);
+        // System.out.println(gcc());
+        // hamiltonian(8);
+        System.out.println(cycleDetection());
         display();
     }
 
@@ -43,7 +46,12 @@ public class mygraph {
         addEdge(5, 6, 7);
         addEdge(5, 7, 6);
         addEdge(6, 7, 6);
-        // removeEdge(4, 5);
+        addEdge(8, 7, 1);// for hamiltonian cycle
+        removeEdge(2, 3);
+        removeEdge(3, 8);
+        removeEdge(4, 5);
+        removeEdge(6, 7);
+
         // removeVertex(5);
 
     }
@@ -154,11 +162,11 @@ public class mygraph {
             return new pair(0, dest + "");
         }
         vis[src] = true;
-        pair ans = new pair(0, "");
+        pair ans = new pair(Integer.MIN_VALUE, "");
         for (Edge e : graph[src]) {
             if (!vis[e.v]) {
                 pair recAns = heaviestPath(e.v, dest, vis);
-                if ( recAns.cost + e.wt > ans.cost) {
+                if (recAns.cost != Integer.MIN_VALUE && recAns.cost + e.wt > ans.cost) {
                     ans.cost = recAns.cost + e.wt;
                     ans.path = src + "" + recAns.path;
                 }
@@ -171,5 +179,265 @@ public class mygraph {
     // public static int kthSmallestpath(int src, int dest) {
 
     // }
+
+    // GET CONNECTED COMPONENETS
+    // METHOD 1 (MARK AT VISITED POINT)
+    public static int gcc() {
+        boolean[] vis = new boolean[n + 1];
+        int compo = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                dfs(i, vis);
+                compo++;
+            }
+        }
+        return compo;
+    }
+
+    public static void dfs(int src, boolean[] vis) {
+        vis[src] = true;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                dfs(e.v, vis);
+            }
+        }
+    }
+
+    // METHOD 2 MART AT GOING POINT
+    public static int gcc2() {
+        boolean[] vis = new boolean[n + 1];
+        int compo = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                dfs2(i, vis);
+                compo++;
+            }
+        }
+        return compo;
+    }
+
+    public static void dfs2(int src, boolean[] vis) {
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                vis[e.v] = true;
+                dfs2(e.v, vis);
+            }
+        }
+    }
+
+    // LEETCOD 1319
+
+    public static int makeConnected(int n, int[][] connections) {
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] rel : connections) {
+            int u = rel[0];
+            int v = rel[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int compo = 0;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, vis);
+                compo++;
+            }
+        }
+
+        return compo - 1;
+    }
+
+    public static void dfs(ArrayList<Integer>[] graph, int src, boolean[] vis) {
+        vis[src] = true;
+        for (int e : graph[src]) {
+            if (!vis[e]) {
+                dfs(graph, e, vis);
+            }
+        }
+
+    }
+
+    // HAMILTONIAN PATH AND CYCLE
+
+    public static void hamiltonian(int src) {
+        boolean[] vis = new boolean[n + 1];
+        hamiltonian(src, src, vis, 1, "");// last 1 is number of vertex visitex
+    }
+
+    public static void hamiltonian(int osrc, int src, boolean[] vis, int count, String psf) {
+        if (count == n) {
+            int idx = findEdge(src, osrc);
+            if (idx == -1) {
+                System.out.println("Hamiltonian Path" + psf + src);
+            } else {
+                System.out.println("Hamiltonian Cycle" + psf + src + osrc);
+            }
+        }
+
+        vis[src] = true;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                hamiltonian(osrc, e.v, vis, count + 1, psf + src);
+            }
+        }
+        vis[src] = false;
+    }
+
+    // CYCLE DETECTION
+
+    // METHOD 1
+
+    public static boolean cycleDetection() {
+        boolean res = false;
+        boolean[] vis = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                res = res || cycleDfs(-1, i, vis);
+            }
+        }
+        return res;
+    }
+
+    public static boolean cycleDfs(int par, int src, boolean[] vis) {
+        vis[src] = true;
+        boolean ans = false;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                ans = ans || cycleDfs(src, e.v, vis);
+            } else if (par != e.v) {
+                return true;
+            }
+        }
+        return ans;
+    }
+
+    // METHOD 2
+    public static boolean cycleDetection2() {
+        boolean res = false;
+        int[] par = new int[n + 1];
+        Arrays.fill(par, -1);
+        for (int i = 1; i <= n; i++) {
+            if (par[i] == -1) {
+                par[i] = i;
+                res = res || cycleDfs2(i, par);
+            }
+        }
+        return res;
+    }
+
+    public static boolean cycleDfs2(int src, int[] par) {
+        boolean ans = false;
+        for (Edge e : graph[src]) {
+            if (par[e.v] == -1) {
+                par[e.v] = src;
+                ans = ans || cycleDfs2(e.v, par);
+            } else if (par[src] != e.v) {
+                return true;
+            }
+        }
+        return ans;
+    }
+
+    // NUMBER OF ISLAND
+
+    public int numIslands(char[][] grid) {
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        int island = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1') {
+                    islandDFS(grid, i, j, dir);
+                    island++;
+                }
+            }
+        }
+        return island;
+    }
+
+    public static void islandDFS(char[][] grid, int sr, int sc, int[][] dir) {
+        grid[sr][sc] = '0';
+        for (int[] d : dir) {
+            int nr = sr + d[0];
+            int nc = sc + d[1];
+            if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == '1') {
+                islandDFS(grid, nr, nc, dir);
+            }
+        }
+    }
+
+    // MAX AREA OF ISLAND
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        int maxArea = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    int cislandArea = islandDFS2(grid, i, j, dir);// current island area
+                    maxArea = Math.max(maxArea, cislandArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    public static int islandDFS2(int[][] grid, int sr, int sc, int[][] dir) {
+        grid[sr][sc] = 0;
+        int count = 0;
+        for (int[] d : dir) {
+            int nr = sr + d[0];
+            int nc = sc + d[1];
+            if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1) {
+                count += islandDFS2(grid, nr, nc, dir);
+            }
+        }
+        return count + 1;
+    }
+
+    // COUNT OF SUB ISLAND
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        int ans = 0;
+        for (int i = 0; i < grid1.length; i++) {
+            for (int j = 0; j < grid1[0].length; j++) {
+                if (grid2[i][j] == 1) {
+                    boolean isSubIsland = subIslandDfs(grid1, grid2, i, j, dir);
+                    if (isSubIsland) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static boolean subIslandDfs(int[][] grid1, int[][] grid2, int sr, int sc, int[][] dir) {
+        boolean ans = true;
+        if (grid2[sr][sc] > grid1[sr][sc]) {
+            ans = false;
+        }
+        grid2[sr][sc] = 0;
+        for (int[] d : dir) {
+            int nr = sr + d[0];
+            int nc = sc + d[1];
+            if (nr >= 0 && nc >= 0 && nr < grid1.length 
+            && nc < grid1[0].length && grid2[nr][nc] == 1) {
+                boolean res = subIslandDfs(grid1, grid2, nr, nc, dir);
+                ans = (ans && res);
+            }
+        }
+        return ans;
+    }
 
 }
