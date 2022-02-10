dc72520dc1d3cfca5f559035b0b20ba59cb0fbc0
kamal <kamal08111998@gmail.com>
2022-02-10 22:25:39 +0530

    Updated

---
+++ GRAPH/DSU/dsu.java
@@ -0,0 +1,36 @@
+public class dsu {
+    int[] rank;
+    int[] par;
+
+    public dsu(int size) {
+        rank = new int[size];
+        par = new int[size];
+        for (int i = 0; i < size; i++) {
+            par[i] = i;
+        }
+    }
+
+    public int find(int u) {
+        return par[u] == u ? u : (par[u] = find(par[u]));
+    }
+
+    public int union(int u, int v) {
+        int l1 = find(u);
+        int l2 = find(v);
+        if (l1 != l2) {
+            int r1 = rank[l1];
+            int r2 = rank[l2];
+            if (r1 > r2) {
+                par[l2] = l1;
+            } else if (r1 < r2) {
+                par[l1] = l2;
+            } else {
+                par[l1] = l2;
+                rank[l2]++;
+            }
+        } else {
+            return -1;
+        }
+        return 1;
+    }
+}
---
+++ GRAPH/DSU/questions.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+public class questions {
+
+    public static class Edge {
+        int v = 0;
+        int wt = 0;
+
+        public Edge(int v, int wt) {
+            this.v = v;
+            this.wt = wt;
+        }
+
+        @Override
+
+        public String toString() {
+            return "(" + v + "," + wt + ") ";
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] edges = { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 0 }, { 4, 2 } };
+        boolean cycle = cycleDetection(5, edges);
+        System.out.println(cycle);
+        ArrayList<Edge>[] graph = SpanningTree(5, edges);
+        display(5, graph);
+    }
+
+    // CYCLE DETECTION
+    public static boolean cycleDetection(int n, int[][] edges) {
+        dsu uf = new dsu(n);
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int val = uf.union(u, v);
+            if (val == -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // SPANNING TREE
+
+    public static ArrayList<Edge>[] SpanningTree(int n, int[][] edges) {
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        init(graph, n);
+        dsu uf = new dsu(n);
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int val = uf.union(u, v);
+            if (val != -1) {
+                addEdge(graph, u, v, 0);
+            }
+        }
+        return graph;
+    }
+
+    public static void init(ArrayList<Edge>[] graph, int n) {
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+    }
+
+    public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int wt) {
+        graph[u].add(new Edge(v, wt));
+        graph[v].add(new Edge(u, wt));
+    }
+
+    public static void display(int n, ArrayList<Edge>[] graph) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + "-> ");
+            for (Edge e : graph[i]) {
+                // System.out.print("(" + e.v + "," + e.wt + ") ");
+                System.out.print(e);
+            }
+            System.out.println();
+        }
+    }
+}
