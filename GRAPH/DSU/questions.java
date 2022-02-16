0aef7319a3ec3775e1d52e0a13c5727bde05f5b5
kamal <kamal08111998@gmail.com>
2022-02-16 23:14:40 +0530

    Update

--- GRAPH/DSU/questions.java
+++ GRAPH/DSU/questions.java
@@ -1,10 +1,9 @@
+import java.lang.Character.UnicodeScript;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-
-import javax.lang.model.type.UnionType;
 
 public class questions {
 
@@ -364,4 +363,246 @@ public class questions {
         }
         return new int[0];
     }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int[] par = new int[26];
+        for (int i = 0; i < 26; i++) {
+            par[i] = i;
+        }
+        // DSU GENERATION
+        for (int i = 0; i < s1.length(); i++) {
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+            int p1 = findPar(par, ch1 - 'a');
+            int p2 = findPar(par, ch2 - 'a');
+            if (p1 < p2) {
+                par[p2] = p1;
+            } else {
+                par[p1] = p2;
+            }
+        }
+
+        // ANSER GENERATION
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : baseStr.toCharArray()) {
+            int idx = ch - 'a';
+            int p = findPar(par, idx);
+            ans.append((char) (p + 'a'));
+        }
+        return ans.toString();
+    }
+
+    public String findPar(HashMap<String, String> par, String u) {
+        if (par.get(u).equals(u)) {
+            return u;
+        }
+        String p = findPar(par, par.get(u));
+        par.put(u, p);
+        return p;
+    }
+
+    // SIMILAR SENTENCE 2
+    public boolean areSentencesSimilarTwo(String[] sentence1, String[] sentence2, List<List<String>> similarPairs) {
+        HashMap<String, String> par = new HashMap<>();
+        if (sentence1.length != sentence2.length) {
+            return false;
+        }
+        // dsu creation
+        for (List<String> similar : similarPairs) {
+            String s1 = similar.get(0);
+            String s2 = similar.get(1);
+            if (!par.containsKey(s1)) {
+                par.put(s1, s1);
+            }
+
+            if (!par.containsKey(s2)) {
+                par.put(s2, s2);
+            }
+
+            String p1 = findPar(par, s1);
+            String p2 = findPar(par, s2);
+            par.put(p1, p2);
+        }
+
+        // CHECK IF SIMILAR
+
+        for (int i = 0; i < sentence1.length; i++) {
+            String s1 = sentence1[i];
+            String s2 = sentence2[i];
+
+            if (s1.equals(s2)) {
+                continue;
+            }
+
+            if (!par.containsKey(s1) || !par.containsKey(s2))
+                return false;
+
+            String p1 = findPar(par, s1);
+            String p2 = findPar(par, s2);
+
+            if (!p1.equals(p2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // SATISFIABLILITY EQUATION
+    public boolean equationsPossible(String[] equations) {
+        int[] par = new int[26];
+        for (int i = 0; i < par.length; i++) {
+            par[i] = i;
+        }
+
+        // dsu for all equal
+        for (String s : equations) {
+            if (s.charAt(1) == '=') {
+                char ch1 = s.charAt(0);
+                char ch2 = s.charAt(3);
+                int p1 = findPar(par, ch1 - 'a');
+                int p2 = findPar(par, ch2 - 'a');
+                par[p1] = p2;
+            }
+        }
+
+        // CHECK FOR OPPOSITE
+        for (String s : equations) {
+            if (s.charAt(1) == '!') {
+                char ch1 = s.charAt(0);
+                char ch2 = s.charAt(3);
+                int p1 = findPar(par, ch1 - 'a');
+                int p2 = findPar(par, ch2 - 'a');
+                if (p1 == p2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        Arrays.sort(edges, (a, b) -> (b[0] - a[0]));
+        int[] apar = new int[n + 1];
+        int[] bpar = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            apar[i] = i;
+            bpar[i] = i;
+        }
+        int remove = 0;
+        int c1 = n;
+        int c2 = n;
+        for (int[] e : edges) {
+            int type = e[0];
+            int u = e[1];
+            int v = e[2];
+            if (type == 1) {// alice
+                int p1 = findPar(apar, u);
+                int p2 = findPar(apar, v);
+                if (p1 == p2) {
+                    remove++;
+                } else {
+                    apar[p1] = p2;
+                    c1--;
+                }
+            } else if (type == 2) {// bob
+                int p1 = findPar(bpar, u);
+                int p2 = findPar(bpar, v);
+                if (p1 == p2) {
+                    remove++;
+                } else {
+                    bpar[p1] = p2;
+                    c2--;
+                }
+            } else {// both
+                int p1 = findPar(apar, u);
+                int p2 = findPar(apar, v);
+                if (p1 == p2) {
+                    remove++;
+                } else {
+                    apar[p1] = p2;
+                    bpar[p1] = p2;
+                    c1--;
+                    c2--;
+                }
+            }
+        }
+        if (c1 != 1 || c2 != 1) {
+            return -1;
+        }
+        return remove;
+    }
+
+    // MALWARE SPREAD
+
+    public int minMalwareSpread(int[][] graph, int[] initial) {
+        int n = graph.length;
+        int[] par = new int[n];
+        int[] size = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            size[i] = 1;
+        }
+
+        // DSU CREATION
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (graph[i][j] == 1) {
+                    int p1 = findPar(par, i);
+                    int p2 = findPar(par, j);
+                    if (p1 != p2) {
+                        par[p1] = p2;
+                        size[p2] += size[p1];
+                    }
+                }
+            }
+        }
+        int[] infected = new int[n];
+        // INFECTED NODE IN PARTICULAR GROUP
+        for (int inf : initial) {
+            int p = findPar(par, inf);
+            infected[p]++;
+        }
+
+        // ANSWER CREATION
+        int maxnodesaved = 0;
+        Arrays.sort(initial);
+        int ans = initial[0];
+
+        for (int inf : initial) {
+            int p = findPar(par, inf);
+            int nodesinfected = infected[p];
+            int sizeofcompo = size[p];
+            if (nodesinfected == 1 && sizeofcompo > maxnodesaved) {
+                ans = inf;
+                maxnodesaved = sizeofcompo;
+            }
+        }
+        return ans;
+    }
+
+    // MIN COST TO CONNECT CITIES
+
+    public int minimumCost(int n, int[][] connections) {
+        int ans = 0;
+        dsu uf = new dsu(n + 1);
+        int compo = n;
+        Arrays.sort(connections, (a, b) -> (a[2] - b[2]));
+        for (int[] e : connections) {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
+            int val = uf.union(u, v);
+            if (val != -1) {
+                ans += wt;
+                compo--;
+            }
+        }
+        if (compo != 1) {
+            return -1;
+        }
+        return ans;
+    }
+
 }
